f50f464a9cf240d68c877eb14a18ae1a029ec874
ahmetzjanov talgat <ahmetzyanov.tv@phystech.edu>
2019-03-23 00:26:02 +0300

    initial app

---
+++ app/src/main/java/four/com/placesrecommendationsonheremap/MainActivity.java
@@ -0,0 +1,13 @@
+package four.com.placesrecommendationsonheremap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
